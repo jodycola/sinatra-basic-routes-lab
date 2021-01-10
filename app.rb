dd32171790c9404bf53bd5e10f57c9a72ecd5ebf
Kody Samaroo <github email address>
2021-01-10 15:19:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        'My name is Kody'
+    end
+
+    get '/hometown' do
+        'My hometown is New Brunswick'
+    end
+
+    get '/favorite-song' do
+        'My favorite song is If I Ruled The World'
+    end
 end
